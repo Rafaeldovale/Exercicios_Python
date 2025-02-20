813fc2589d2c2ae8369d63417d10fe4cfb2ebfcc
rafaeldovale666 <phaelduvale@hotmail.com>
2025-02-20 05:21:09 -0300

    Removendo pasta Estrutura_Repetição

--- Estrutura_Repetição/exe01.py
+++
@@ -1,9 +0,0 @@
-num = 0
-
-while True:
-    num = int(input('Informe um valor de 0 até 10: '))
-
-    if 0 <= num <= 10:
-        break
-    else:
-        print('Valor inválido')
--- Estrutura_Repetição/exe02.py
+++
@@ -1,9 +0,0 @@
-
-while True:
-    nome = str(input('Informe seu nome: '))
-    senha = str(input('Informe sua senha: '))
-
-    if nome == senha:
-        print('Senhá Inválida!')
-    else:
-        break
